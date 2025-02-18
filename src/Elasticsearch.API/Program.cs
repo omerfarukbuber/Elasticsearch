104ec2d232c3ec2dec1756b45d0c9ad9d3e93d19
Ömer Faruk BÜBER <omerfbuber@gmail.com>
2025-02-18 21:03:37 +0300

    Add swagger ui

--- src/Elasticsearch.API/Program.cs
+++ src/Elasticsearch.API/Program.cs
@@ -12,6 +12,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "OpenAPI V1");
+    });
 }
 
 app.UseHttpsRedirection();
