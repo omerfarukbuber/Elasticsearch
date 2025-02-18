3574541d42e0159908d04d7eae1550a84ac43187
Ömer Faruk BÜBER <omerfbuber@gmail.com>
2025-02-18 22:52:14 +0300

    Create product and product feature entities

---
+++ src/Elasticsearch.API/Features/Products/Colors.cs
@@ -0,0 +1,8 @@
+﻿namespace Elasticsearch.API.Features.Products;
+
+public enum Colors
+{
+    Red = 1,
+    Green = 2,
+    Blue = 3
+}
---
+++ src/Elasticsearch.API/Features/Products/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Elasticsearch.API.Features.Products;
+
+public sealed class Product
+{
+    [JsonPropertyName("_id")]
+    public string Id { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime? Updated { get; set; }
+    public ProductFeature? ProductFeature { get; set; }
+}
