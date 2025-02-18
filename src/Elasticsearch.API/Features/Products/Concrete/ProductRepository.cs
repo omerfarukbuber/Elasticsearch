ee11a6d777a67e2bfb54dacbe34f92eee865cd1c
Ömer Faruk BÜBER <omerfbuber@gmail.com>
2025-02-18 22:54:46 +0300

    Create repository and service classes which implements the interfaces

---
+++ src/Elasticsearch.API/Features/Products/Concrete/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using Elastic.Clients.Elasticsearch;
+using Elasticsearch.API.Extensions;
+using Elasticsearch.API.Features.Products.Abstract;
+
+namespace Elasticsearch.API.Features.Products.Concrete;
+
+public class ProductRepository(ElasticsearchClient client, DateTimeProvider dateTimeProvider) : IProductRepository
+{
+    private const string IndexName = "products";
+    public async Task<Product?> SaveAsync(Product entity)
+    {
+        entity.Created = dateTimeProvider.UtcNow;
+
+        var response = await client.IndexAsync(entity, x => x.Index(IndexName));
+
+        if (!response.IsValidResponse) return null;
+
+        entity.Id = response.Id;
+        return entity;
+    }
+}
---
+++ src/Elasticsearch.API/Features/Products/Concrete/ProductService.cs
@@ -0,0 +1,15 @@
+﻿using Elasticsearch.API.Features.Products.Abstract;
+
+namespace Elasticsearch.API.Features.Products.Concrete;
+
+public class ProductService(IProductRepository repository) : IProductService
+{
+    public async Task<ProductResponse?> CreateAsync(CreateProductRequest request)
+    {
+        var product = request.ToProduct();
+        var response = await repository.SaveAsync(product);
+        return response is null
+            ? null
+            : ProductResponse.FromProduct(response);
+    }
+}
