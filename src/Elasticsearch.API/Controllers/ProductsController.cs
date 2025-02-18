72e54607824929e408bc9a7e882e8220ee003520
Ömer Faruk BÜBER <omerfbuber@gmail.com>
2025-02-18 22:55:44 +0300

    Create products controller and add create endpoint

---
+++ src/Elasticsearch.API/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Elasticsearch.API.Features.Products;
+using Elasticsearch.API.Features.Products.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Elasticsearch.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProductsController : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> CreateProduct([FromBody] CreateProductRequest request, IProductService productService)
+    {
+        var result = await productService.CreateAsync(request);
+        return result is null
+            ? BadRequest()
+            : Created("", result);
+    }
+
+} 
