8bd03a7ddc87e450f8ec89fbbd5ea8564cc390d8
Ömer Faruk BÜBER <omerfbuber@gmail.com>
2025-02-19 13:35:11 +0300

    Create a Controller for search operations

---
+++ src/Elasticsearch.API/Controllers/ECommerceController.cs
@@ -0,0 +1,18 @@
+﻿using Elasticsearch.API.Features.ECommerce.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Elasticsearch.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ECommerceController(IECommerceRepository repository) : ControllerBase
+    {
+        [HttpGet("term/customer_first_name/{customerFirstName}")]
+        public async Task<IActionResult> GetByCustomerFirstName([FromRoute] string customerFirstName)
+        {
+            var result = await repository.TermSearchByCustomerFirstNameAsync(customerFirstName);
+            return Ok(result);
+        }
+    }
+}
