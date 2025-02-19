82263e7d78d3c67ff447a7782f11ae1475c38b1b
Ömer Faruk BÜBER <omerfbuber@gmail.com>
2025-02-19 13:33:12 +0300

    Create the entity files used in a sample Elasticsearch index.

---
+++ src/Elasticsearch.API/Features/ECommerce/ECommerceProduct.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Elasticsearch.API.Features.ECommerce;
+
+public sealed class ECommerceProduct
+{
+    [JsonPropertyName("product_id")]
+    public long ProductId { get; set; }
+
+    [JsonPropertyName("product_name")] 
+    public string ProductName { get; set; } = null!;
+}
