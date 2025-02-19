c50ca3a1531714f6d0b9dd1913a7153a977a8b3e
Ömer Faruk BÜBER <omerfbuber@gmail.com>
2025-02-19 13:34:51 +0300

    Add repository to DI container

--- src/Elasticsearch.API/Program.cs
+++ src/Elasticsearch.API/Program.cs
@@ -1,4 +1,6 @@
 using Elasticsearch.API.Extensions;
+using Elasticsearch.API.Features.ECommerce.Abstract;
+using Elasticsearch.API.Features.ECommerce.Concrete;
 using Elasticsearch.API.Features.Products.Abstract;
 using Elasticsearch.API.Features.Products.Concrete;
 
@@ -15,6 +17,7 @@ builder.Services.AddElasticsearch(builder.Configuration);
 builder.Services.AddTransient(sp => new DateTimeProvider());
 builder.Services.AddTransient<IProductRepository, ProductRepository>();
 builder.Services.AddTransient<IProductService, ProductService>();
+builder.Services.AddTransient<IECommerceRepository, ECommerceRepository>();
 
 var app = builder.Build();
 
